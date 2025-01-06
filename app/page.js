f75ddc11bdb2cdb3ed5036564b052152ce48cb09
unknown <s2lemoinenathalan@gmail.com>
2025-01-06 11:49:16 +0100

    ok

--- app/page.js
+++ app/page.js
@@ -14,7 +14,6 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-           Macdo recrute
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.js
             </code>
